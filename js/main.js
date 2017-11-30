07354f0b443ee7aa464a6eb6a11e0629ab15e6a3
fly <franck.ly247@gmail.com>
2017-11-29 23:49:19 -0500

    fix empty string

--- js/main.js
+++ js/main.js
@@ -24,35 +24,35 @@ function createElement(inputValue, checked = false, setStorage = false) {
     alert("You must write something!");
   } else {
     document.getElementById("list").appendChild(li);
-  }
-  document.getElementById("todo").value = "";
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\u00D7");
-  var id = `${myTodos.length}-todo`;
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  li.id = id;
-  li.onclick = function() {
-    var index = indexOf(id);
-    if (index > -1) {
-      myTodos[index].checked = !myTodos[index].checked;
+    document.getElementById("todo").value = "";
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    var id = `${myTodos.length}-todo`;
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    li.id = id;
+    li.onclick = function() {
+      var index = indexOf(id);
+      if (index > -1) {
+        myTodos[index].checked = !myTodos[index].checked;
+        localStorage.setItem('storedTodos', JSON.stringify(myTodos));
+        li.classList.toggle('checked');
+      }
+    }
+    span.onclick = function() {
+      document.getElementById(id).remove();
+      myTodos.splice(indexOf(id), 1)
       localStorage.setItem('storedTodos', JSON.stringify(myTodos));
+    }
+    if (checked) {
       li.classList.toggle('checked');
     }
-  }
-  span.onclick = function() {
-    document.getElementById(id).remove();
-    myTodos.splice(indexOf(id), 1)
-    localStorage.setItem('storedTodos', JSON.stringify(myTodos));
-  }
-  if (checked) {
-    li.classList.toggle('checked');
-  }
-  myTodos.push({ id, todo: inputValue, checked });
+    myTodos.push({ id, todo: inputValue, checked });
 
-  if (setStorage) {
-    localStorage.setItem('storedTodos', JSON.stringify(myTodos));
+    if (setStorage) {
+      localStorage.setItem('storedTodos', JSON.stringify(myTodos));
+    }
   }
 }
 
